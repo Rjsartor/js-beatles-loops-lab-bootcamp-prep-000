f177b357072715f14f98b85bd24afd871ed7df18
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-16 19:20:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musiciansArray, instrumentsArray) {
-  newArray = []
+ let newArray = []
   musiciansArray.forEach((musician) => {
     instrumentsArray.forEach((instrument) => {
       newArray.push(`${musician[0]} plays ${instrument[0]}`)
